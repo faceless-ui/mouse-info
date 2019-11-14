e837870ec30e1b4f3d915f7f0ed2d2a3c1e7e8b6
Jacob Fletcher <jacobsfletch@gmail.com>
2019-11-13 21:28:52 -0500

    exposes MousePositionContext

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
+export { default as MousePositionContext } from './MousePositionProvider/context';
 export { default as MousePositionProvider } from './MousePositionProvider';
 export { default as withMousePosition } from './withMousePosition';
