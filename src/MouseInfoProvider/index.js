9f0bf486ce3b2ab8b992b7cafddd5d4ffc3ebb2b
Jarrod Flesch <jarrodmflesch@gmail.com>
2019-12-07 02:43:14 -0500

    removes Component from import

--- src/MouseInfoProvider/index.js
+++ src/MouseInfoProvider/index.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import MouseInfoContext from './context';
 
