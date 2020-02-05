261fbb7d88d559eeb27385572732d1aef84876c1
Jacob Fletcher <jacobsfletch@gmail.com>
2020-02-04 22:42:55 -0500

    retrofits @trbl/utils into testing suite

--- src/index.test.js
+++ src/index.test.js
@@ -1,23 +1,8 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { mockWindowMouseEvents, mockRequestAnimationFrame } from '@trbl/utils';
 
 configure({ adapter: new Adapter() });
 
-window.document.mouseTo = function mouseTo(xCoord, yCoord) {
-  if (xCoord <= window.innerWidth && yCoord <= window.innerHeight) {
-    const event = new window.Event('mousemove');
-    event.clientX = xCoord;
-    event.clientY = yCoord;
-
-    window.document.dispatchEvent(event);
-  }
-};
-
-window.document.mouseEnter = () => window.document.dispatchEvent(new window.Event('mouseenter'));
-
-window.document.mouseLeave = () => window.document.dispatchEvent(new window.Event('mouseleave'));
-
-window.requestAnimationFrame = (callback) => {
-  const timestamp = performance.now();
-  callback(timestamp);
-};
+mockWindowMouseEvents();
+mockRequestAnimationFrame();
