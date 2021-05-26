a5054dcff56dc75f72e12fdaf223cb77dcb3271a
Jacob Fletcher <jacobsfletch@gmail.com>
2021-05-26 01:26:06 -0400

    chore: migrates MouseInfoProvider to functional component

--- src/MouseInfoContext/types.ts
+++ src/MouseInfoContext/types.ts
@@ -10,5 +10,4 @@ export interface IMouseInfoContext {
   totalPercentage: number,
   isInViewport: boolean,
   eventsFired: number,
-  animationScheduled: boolean,
 }
--- src/MouseInfoProvider/index.tsx
+++ src/MouseInfoProvider/index.tsx
@@ -1,102 +1,127 @@
-import React, { Component } from 'react';
+import React, {
+  useCallback,
+  useEffect,
+  useReducer,
+  useRef,
+  useState,
+} from 'react';
 import MouseInfoContext from '../MouseInfoContext';
 import { IMouseInfoContext } from '../MouseInfoContext/types';
 import { Props } from './types';
 
-class MouseInfoProvider extends Component<Props, IMouseInfoContext> {
-  constructor(props: Props) {
-    super(props);
+const reducer = (
+  state: IMouseInfoContext,
+  payload: {
+    e?: MouseEvent,
+    timestamp?: number,
+    animationRef: React.MutableRefObject<number>
+  },
+) => {
+  const {
+    e,
+    timestamp,
+    animationRef,
+  } = payload;
 
-    this.state = {
-      x: 0,
-      y: 0,
-      xDifference: 0,
-      yDifference: 0,
-      xDirection: undefined,
-      yDirection: undefined,
-      xPercentage: 0,
-      yPercentage: 0,
-      totalPercentage: 0,
-      isInViewport: false,
-      eventsFired: 0,
-      animationScheduled: false,
-    };
-  }
+  animationRef.current = undefined;
+  const {
+    x: prevMouseX,
+    y: prevMouseY,
+    xDirection: prevXDirection,
+    yDirection: prevYDirection,
+    eventsFired,
+  } = state;
 
-  componentDidMount(): void {
-    document.addEventListener('mousemove', this.requestAnimation);
-    document.addEventListener('mouseenter', () => this.setViewportStatus(true));
-    document.addEventListener('mouseleave', () => this.setViewportStatus(false));
-  }
+  const currentMouseX = e.clientX;
+  const currentMouseY = e.clientY;
 
-  componentWillUnmount(): void {
-    document.removeEventListener('mousemove', this.requestAnimation);
-    document.removeEventListener('mouseenter', () => this.setViewportStatus);
-    document.removeEventListener('mouseleave', () => this.setViewportStatus);
-  }
+  const xDifference = currentMouseX - prevMouseX;
+  const yDifference = currentMouseY - prevMouseY;
 
-  requestAnimation = (e: MouseEvent): void => {
-    const { animationScheduled } = this.state;
+  const xPercentage = (currentMouseX / window.innerWidth) * 100;
+  const yPercentage = (currentMouseY / window.innerHeight) * 100;
+  const totalPercentage = (xPercentage + yPercentage) / 2;
 
-    if (!animationScheduled) {
-      this.setState({
-        animationScheduled: true,
-      }, () => requestAnimationFrame((timestamp) => this.updateMouseInfo(e, timestamp)));
-    }
-  }
+  const xDirection = xDifference > 0 ? 'right' : xDifference < 0 ? 'left' : prevXDirection; // eslint-disable-line no-nested-ternary
+  const yDirection = yDifference > 0 ? 'down' : yDifference < 0 ? 'up' : prevYDirection; // eslint-disable-line no-nested-ternary
 
-  setViewportStatus = (status: MouseEvent | boolean): void => {
-    this.setState({ isInViewport: Boolean(status) });
-  }
-
-  updateMouseInfo = (e: MouseEvent, timestamp?: number): void => {
-    const {
-      x: prevMouseX,
-      y: prevMouseY,
-      xDirection: prevXDirection,
-      yDirection: prevYDirection,
-      eventsFired,
-    } = this.state;
-
-    const currentMouseX = e.clientX;
-    const currentMouseY = e.clientY;
-
-    const xDifference = currentMouseX - prevMouseX;
-    const yDifference = currentMouseY - prevMouseY;
-
-    const xPercentage = (currentMouseX / window.innerWidth) * 100;
-    const yPercentage = (currentMouseY / window.innerHeight) * 100;
-    const totalPercentage = (xPercentage + yPercentage) / 2;
-
-    const xDirection = xDifference > 0 ? 'right' : xDifference < 0 ? 'left' : prevXDirection; // eslint-disable-line no-nested-ternary
-    const yDirection = yDifference > 0 ? 'down' : yDifference < 0 ? 'up' : prevYDirection; // eslint-disable-line no-nested-ternary
-
-    this.setState({
-      x: currentMouseX,
-      y: currentMouseY,
-      xDifference,
-      yDifference,
-      xDirection,
-      yDirection,
-      xPercentage,
-      yPercentage,
-      totalPercentage,
-      eventsFired: timestamp ? eventsFired + 1 : eventsFired,
-      animationScheduled: false,
-    });
+  return {
+    x: currentMouseX,
+    y: currentMouseY,
+    xDifference,
+    yDifference,
+    xDirection,
+    yDirection,
+    xPercentage,
+    yPercentage,
+    totalPercentage,
+    eventsFired: timestamp ? eventsFired + 1 : eventsFired,
   };
+};
 
-  render(): JSX.Element {
-    const { children } = this.props;
-    const mouseInfo = { ...this.state };
-    delete mouseInfo.animationScheduled;
+const MouseInfoProvider: React.FC<Props> = (props) => {
+  const {
+    children,
+  } = props;
 
-    return (
-      <MouseInfoContext.Provider value={{ ...mouseInfo }}>
-        {children && children}
-      </MouseInfoContext.Provider>
+  const animationRef = useRef<number>(null);
+
+  const [state, dispatch] = useReducer(reducer, {
+    x: 0,
+    y: 0,
+    xDifference: 0,
+    yDifference: 0,
+    xDirection: undefined,
+    yDirection: undefined,
+    xPercentage: 0,
+    yPercentage: 0,
+    totalPercentage: 0,
+    eventsFired: 0,
+  });
+
+  const [isInViewport, setIsInViewport] = useState(undefined);
+
+  const requestAnimation = useCallback((e?: MouseEvent): void => {
+    if (animationRef.current) cancelAnimationFrame(animationRef.current);
+    animationRef.current = requestAnimationFrame(
+      (timestamp) => {
+        dispatch({
+          e,
+          timestamp,
+          animationRef,
+        });
+      },
     );
-  }
-}
+  }, []);
+
+  const setViewportStatus = useCallback((status: MouseEvent | boolean): void => {
+    setIsInViewport(Boolean(status));
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener('mousemove', requestAnimation);
+    document.addEventListener('mouseenter', () => setViewportStatus(true));
+    document.addEventListener('mouseleave', () => setViewportStatus(false));
+    return () => {
+      document.removeEventListener('mousemove', requestAnimation);
+      document.removeEventListener('mouseenter', () => setViewportStatus);
+      document.removeEventListener('mouseleave', () => setViewportStatus);
+    };
+  }, [
+    requestAnimation,
+    setViewportStatus,
+  ]);
+
+  return (
+    <MouseInfoContext.Provider
+      value={{
+        ...state,
+        isInViewport,
+      }}
+    >
+      {children && children}
+    </MouseInfoContext.Provider>
+  );
+};
 
 export default MouseInfoProvider;
