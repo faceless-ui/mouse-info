91b85b8ff2b838c1cff62eb046f543c967085542
Jacob Fletcher <jacobsfletch@gmail.com>
2024-06-08 10:49:52 -0400

    chore: nodenext module resolution

--- demo/UseMouseInfo.demo.tsx
+++ demo/UseMouseInfo.demo.tsx
@@ -1,5 +1,5 @@
 import { useMouseInfo } from '@faceless-ui/mouse-info';
-import LogProps from './LogProps';
+import LogProps from './LogProps.js';
 
 const UseMouseInfo: React.FC = () => {
   const mouseInfo = useMouseInfo();
